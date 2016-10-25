ed48f25473bdcdefea3b144af53c13761f27336a
dnlcrl <ciriello.daniele@gmail.com>
2016-10-25 11:19:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+[![No Maintenance Intended](http://unmaintained.tech/badge.svg)](http://unmaintained.tech/)
+
+# ig-bot 
+
+## Python3 Instagram grey hat bot (for demonstration purposes only).
+
+This is an instagram bot for demonstration purposes, it does the following:
+
+### Likes and Follows
+
+- select a favourite hashtag
+- search for medias with that hashtag
+- for each found recent media (unless ~20 people are followed)
+	- if the author has less than FOLLOWERS_THRESHOLD followers
+		- like its most recents medias (~10)
+		- follow that user
+- sleep an hour circa
+
+### Image Posting
+
+
+- the bot will post an image on each scheduled time, the description will be the original page title plus the chosen hashtag plus some related hashtag
+
+### Log on Twitter
+
+The bot will tweet whenever it follows an user, sleep after some follows, post an image or when it crash.
+
+# Usage
+
+phase 1:
+- choose images to post with the chrome extension, right click on it -> Gram It Later -> select the hashtag
+phase 2:
+- run python bot/main.py
+- check your twitter account or your terminale
+
+# Setup
+
+- hashtags files: you need a file 'hashtags' and 'relatedhashtags' in this directory, containing one hashtag for line.
+
+- put the relative exports in your .profile for instagram credentials, twitter tokens, and the gram it later folder.
+
+- open the movefiles workflows with Automator and set the Chrome's download folder and the gramitlater folder (if you aren't on MacOS, create a script to move gramitlater files after download or set the gramitlater folder as your chrome's download folder).
+
+# Requirements
+
+- python3
+- tweepy
+- Instagram-API-Python
+
+# Why?
+
+because I love coding and automating things.
+
+# Need Help?
+
+Drop me a line: [@dnlcrl](http://twitter.com/dnlcrl)
---
+++ pyig/main.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+'''
+Best Times to Post on Instagram by Day of the Week
+Sunday: 5:00 p.m. -> 11:00 PM -> 23:00
+Monday: 7:00 p.m. & 10:00 p.m. -> 1 am & 4 am -> 01:00 $ 04:00
+Tuesday: 3:00 a.m. & 10:00 p.m. -> 9 pm & 4 am -> 21:00 $ 04:00
+Wednesday: 5:00 p.m. -> 11:00 PM  -> 23:00
+Thursday: 7:00 a.m. & 11:00 p.m. -> 1 am & 5 am  -> 01:00 & 05:00
+Friday: 1:00 a.m. & 8:00 p.m. -> 7 pm & 2 am -> 19:00 & 02:00
+Saturday: 12:00 a.m. & 2:00 a.m. -> 6:00 am & 8:00 am -> 06:00 & 08:00
+'''
+
+
+from InstagramAPI import InstagramAPI
+from random import randint, uniform, choice
+from time import sleep
+import tweepy
+from datetime import datetime
+import os
+from urllib import request
+from random import shuffle
+
+CONSUMER_KEY = os.environ.get('TW_CONSUMER_KEY')
+CONSUMER_SECRET = os.environ.get('TW_CONSUMER_SECRET')
+ACCESS_TOKEN = os.environ.get('TW_ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.environ.get('TW_ACCESS_TOKEN_SECRET')
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+FOLLOWERS_THRESHOLD = 2 * 1000
+GRAMITLATER_DIR = os.environ.get('GRAMITLATER_DIR')
+
+IG_USER = os.environ.get('IG_USER')
+IG_PASS = os.environ.get('IG_PASS')
+
+SCHEDULE = {
+    6: (23,),
+    0: (None,),
+    1: (1, 4, 21,),
+    2: (4, 23,),
+    3: (None,),
+    4: (1, 5, 19,),
+    5: (2, 6, 8,)
+}
+
+app = {}
+
+# load hashtags
+with open('hashtags', 'r') as f:
+    hashtags = f.read().splitlines()
+with open('relatedhashtags', 'r') as f:
+    relatedhashtags = f.read().splitlines()
+shuffle(relatedhashtags)
+
+# load following users
+with open('following', 'r') as f:
+    following = f.read().splitlines()
+
+following = [int(fo) for fo in following]
+
+
+def about_an_hour():
+    return 5400 + randint(-1800, 1800)
+
+
+def about_a_minute():
+    return 90 + randint(-30, 30)
+
+
+def about_a_second():
+    return uniform(1.5, 2.6)
+
+
+try:
+    ig = InstagramAPI(IG_USER, IG_PASS)
+    ig.login()  # login
+except Exception as e:
+    raise e
+
+
+def maybe_post_update():
+    wd = datetime.today().weekday()
+    h = datetime.today().hour
+    if 'wd' in app and app['wd'] == wd:
+        if 'h' in app and app['h'] == h:
+            return
+    if h in SCHEDULE[wd]:
+        post_updte()
+        app['wd'] = wd
+        app['h'] = h
+
+
+def post_updte():
+    _dir = GRAMITLATER_DIR
+    gram_files = [i for i in os.listdir(_dir) if i.startswith('gramitlater')]
+    if len(gram_files) == 0:
+        print('No more grams!')
+        return
+    shuffle(gram_files)
+    g = gram_files[0]
+    with open(_dir + g, 'r') as f:
+        content = f.read()
+        g_els = content.split('\n')
+        fname = 'media.' + g_els[2].split('.')[-1]
+        request.urlretrieve(g_els[2], fname)
+        f = request.urlopen(g_els[1]).read()
+        tags = ''.join(
+            ['#' + t + ' ' for t in relatedhashtags[:randint(2, 4)]])
+        caption = g_els[0] + ' #' + g_els[3] + ' ' + tags
+        try:
+            ig.uploadPhoto(fname, caption)
+            os.remove(_dir + g)
+            api.update_status(status='%s: 📸' % datetime.now())
+        except Exception as e:
+            import pdb
+            pdb.set_trace()
+            raise e
+
+
+def like_and_follow():
+    tag = choice(hashtags)
+    print('Tag selected: ', tag)
+    ig.tagFeed(tag)
+    sleep(about_a_second())
+
+    response = ig.LastJson
+
+    items = response["items"]
+    users_ids = [item["user"]["pk"] for item in items]
+    users_ids = [_id for _id in users_ids if _id not in following]
+
+    for user_id in users_ids[:randint(10, 15)]:
+        # numero di follower
+        ig.getUsernameInfo(user_id)
+        sleep(about_a_second())
+        follower_count = ig.LastJson['user']['follower_count']
+        print('Follower Conut: ', follower_count)
+
+        if follower_count < FOLLOWERS_THRESHOLD:
+
+            ig.getUserFeed(user_id)
+            sleep(about_a_second())
+            feed = ig.LastJson['items']
+            _ids = [el['id'] for el in feed][:randint(7, 11)]
+            for post_id in _ids:
+                ig.like(post_id)
+                print('Post ', post_id, ' liked!')
+                #.update_status(status='%s: 👍' % datetime.now())
+                sleep(about_a_second())
+            ig.follow(user_id)
+            following.append(user_id)
+            with open('following', 'a') as f:
+                f.write('\n' + str(user_id))
+
+            print('User ', user_id, ' followed!')
+            try:
+                api.update_status(status='%s: 👣' % datetime.now())
+            except Exception as e:
+                print('Tweepy error:')
+                print(e)
+            app['count'] += 1
+            print('sleeping a minute or so..')
+            sleep(about_a_minute() * 0.75)
+    if app['count'] >= app['folbeforesleep']:
+        app['folbeforesleep'] = randint(18, 22)
+        api.update_status(status='%s: 😴' % datetime.now())
+        print('sleeping an hour or so..')
+        sleep(about_an_hour())
+
+
+def main():
+    app['count'] = 0
+    app['folbeforesleep'] = randint(18, 22)
+    while True:
+        try:
+            maybe_post_update()
+            like_and_follow()
+
+        except Exception as e:
+            api.update_status(status='%s: 🔥🔥🔥' % datetime.now())
+            raise e
+
+if __name__ == '__main__':
+    main()
